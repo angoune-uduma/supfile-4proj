e1f9f65b05275514a0d54931f066068e910a9500
Idika-lionnel <idika-lionnel.angoune-uduma@supinfo.com>
2026-02-24 22:01:01 +0100

    organisation apres push raté

--- supfile-4proj-main/supfile-web/backend/src/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-async function connectDB(uri) {
-  mongoose.set("strictQuery", true);
-  await mongoose.connect(uri);
-  console.log("✅ MongoDB connecté");
-}
-
-module.exports = { connectDB };
--- supfile-4proj-main/supfile-web/backend/src/routes/files.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const auth = require("../middlewares/auth.middleware");
-const upload = require("../middlewares/upload.middleware");
-const files = require("../controllers/files.controller");
-
-router.post("/", auth, upload.single("file"), files.upload);
-
-module.exports = router;
