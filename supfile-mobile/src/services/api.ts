73f7d50826c4787e41d1d6dab52a8f499222b88c
jeff <jeffreyburluraux@gmail.com>
2026-04-19 20:19:33 +0200

    lancement mobile

--- supfile-mobile/src/services/api.ts
+++ supfile-mobile/src/services/api.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { getAccessToken } from "./secureStore"; // <-- IMPORTANT
 
 const API_URL =
-  process.env.EXPO_PUBLIC_API_URL ||
-  "http://192.168.1.163:4000"; // <-- ton IP Mac
+  process.env.EXPO_PUBLIC_API_URL || "http://172.20.10.3:4000"; // <-- ton IP Mac
+
 
 export const api = axios.create({
   baseURL: API_URL,
--- supfile-mobile/src/services/secureStore.ts
+++ supfile-mobile/src/services/secureStore.ts
@@ -1,22 +1,43 @@
 import * as SecureStore from "expo-secure-store";
+import { Platform } from "react-native";
 
 const ACCESS_KEY = "supfile_access_token";
 const REFRESH_KEY = "supfile_refresh_token";
 
 export async function setTokens(access: string, refresh?: string) {
+  if (Platform.OS === "web") {
+    localStorage.setItem(ACCESS_KEY, access);
+    if (refresh) localStorage.setItem(REFRESH_KEY, refresh);
+    return;
+  }
+
   await SecureStore.setItemAsync(ACCESS_KEY, access);
   if (refresh) await SecureStore.setItemAsync(REFRESH_KEY, refresh);
 }
 
 export async function clearTokens() {
+  if (Platform.OS === "web") {
+    localStorage.removeItem(ACCESS_KEY);
+    localStorage.removeItem(REFRESH_KEY);
+    return;
+  }
+
   await SecureStore.deleteItemAsync(ACCESS_KEY);
   await SecureStore.deleteItemAsync(REFRESH_KEY);
 }
 
 export async function getAccessToken() {
+  if (Platform.OS === "web") {
+    return localStorage.getItem(ACCESS_KEY);
+  }
+
   return SecureStore.getItemAsync(ACCESS_KEY);
 }
 
 export async function getRefreshToken() {
+  if (Platform.OS === "web") {
+    return localStorage.getItem(REFRESH_KEY);
+  }
+
   return SecureStore.getItemAsync(REFRESH_KEY);
 }
